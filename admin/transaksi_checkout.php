1da940d3508e8c9bdac7b68977dafe9084c19980
Donny Akhmad Septa Utama <donny.akhmad.septa.utama26@gmail.com>
2023-08-01 20:29:55 +0700

    fix: update nokamar setelah checkout

--- admin/transaksi_checkout.php
+++ admin/transaksi_checkout.php
@@ -10,4 +10,12 @@ $update_checkin = "
     where id_transaksi='$trx_id'";
 mysqli_query($koneksi, $update_checkin);
 
+$query = "select * from punya where id_transaksi='$trx_id'";
+$get_punya = mysqli_query($koneksi, $query);
+$punya = mysqli_fetch_assoc($get_punya);
+$nokamar = $punya['nokamar'];
+
+$update_nokamar = "update nokamar set kosong='Y' where nokamar='$nokamar'";
+mysqli_query($koneksi, $update_nokamar);
+
 header("location:transaksi.php");
